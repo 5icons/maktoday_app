0eddece41e6419529da2574fb6e26b814caf02a7
Yusuf Anyanzwa <yusuf_5icons@yahoo.com>
2013-01-18 09:22:18 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MaktodayApp::Application.config.secret_token = 'c41b3a551ca524a958ca1920312da1e5915582e98de18423cd4ae1d21cb1c69eea095d7a9aee4431f448ef24d3ad9a8bf01093ff050971489d736c9b71440dbf'
